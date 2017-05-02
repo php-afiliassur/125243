38ed42ba34a7ee2c63b5532eb223e354df99ac15
root <root@template.ovh.com>
2017-05-02 18:18:36 +0200

    go

--- master/config.php
+++ master/config.php
@@ -1,19 +1,13 @@
 <?php
-#$host = 'regions-departements.com';
-#$return_path = 'root@'.$host;
-#$source_email = 'contact@'.$host;
-#$replyto_email = 'contact@'.$host;
-
+$email_from = $argv[5];
 $email_subject = $argv[4];
-$source_displayname  = $argv[5];
-$replyto_displayname = $argv[5];
+$source_displayname = $argv[6];
 
 $callback = 'handle.php';
 $landingpage = 'test';
 
 $phrase_desabonnement = 'suivez ce lien pour votre désinscription.';
 $db_file = file('./database/'.$argv[1]);
-$email_body = file_get_contents('./template/'.$argv[2].'.htm');
-$email_altbody = file_get_contents('./template/'.$argv[2].'.txt');
+$template = fopen('./template/'.$argv[2], 'r');
 $pool = fopen('./pool/'.$argv[3], 'r');
-$limit_email=2;
+$limit_email=4;
--- master/phpmailer.php
+++ master/phpmailer.php
@@ -4,56 +4,56 @@ set_time_limit(0);
 require_once './config.php';
 require_once './lib/PHPMailer/PHPMailerAutoload.php';
 
-$host=fgets($pool);
-
-$mail = new PHPMailer;
-$mail->Host = $host;
-//$mail->SMTPDebug = 3;
-$mail->isSMTP();
-$mail->SMTPAuth = false;
-$mail->Port = 25; 
-$mail->SMTPKeepAlive = true;
 $email_counter=0;
-
 
 foreach($db_file as $email_to) {
 
-$mail->setFrom('contact@'.$host, $source_displayname);
-$mail->addReplyTo('contact@'.$host, $replyto_displayname);
+// generation du host
+$host=fgets($pool);
+$email_body = file_get_contents('./template/'.trim(fgets($template)));
+
+$mail = new PHPMailer;
+$mail->Host = $host;
+$mail->isSMTP();
+$mail->SMTPAuth = false;
+$mail->Port = 25;
+$mail->SMTPKeepAlive = true;
+$mail->setFrom($email_from.'@'.$host, $source_displayname);
+$mail->addReplyTo($email_from.'@'.$host, $source_displayname);
 $mail->sender = 'root@'.$host;
 $mail->isHTML(true);
 $mail->CharSet = 'UTF-8';
 
 echo "Envoi en cours ...".PHP_EOL;
+
 $tag_unsubscribe = '<a href="http://'.$host.'/'.$callback.'?type=2&email='.base64_encode($email_to).'">'.$phrase_desabonnement.'</a>';
 $tag_opener      = '<img src="http://'.$host.'/'.$callback.'?type=1&email='.base64_encode($email_to).'" alt="" height="1" width="1"></img>';
 $tag_clicker     = 'http://'.$host.'/'.$callback.'?type=3&l='.$landingpage.'&email='.base64_encode($email_to).'';
 
 $email_body_final = str_replace('DESABONNEMENT', $tag_unsubscribe, $email_body);
-$email_body_final = str_replace('OPENER', $tag_opener, $email_body_final);
-$email_body_final = str_replace('click', $tag_clicker, $email_body_final);
+$email_body_final = str_replace('OPENR', $tag_opener, $email_body_final);
+$email_body_final = str_replace('CLICKR', $tag_clicker, $email_body_final);
 
 $mail->addAddress(trim($email_to));
 $mail->Subject = $email_subject;
 $mail->Body    = $email_body_final;
-$mail->AltBody = $email_altbody;
+$mail->AltBody = "";
 $mail->send();
 $mail->clearAddresses();
+
 echo $email_to.PHP_EOL;
 
 $email_counter++;
-
 if($email_counter>$limit_email){
-
-  $host=fgets($pool);
+  $host=fgets($pool); // new host
   $mail = new PHPMailer;
   $mail->Host = $host;
   $mail->isSMTP();
   $mail->SMTPAuth = false;
   $mail->Port = 25;
   $mail->SMTPKeepAlive = true;
-  $email_counter=0;
 
-  print_r($host);
+  $email_body = file_get_contents('./template/'.fgets($template));
+  $email_counter=0;
 }
 }
