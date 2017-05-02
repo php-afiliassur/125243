525371ba2b7ae018c4c4fea9e182c3030ceafd25
php-afiliassur <afiliassur@wanadoo.fr>
2017-05-02 16:35:19 +0200

    Update deploy.sh

--- master/deploy/deploy.sh
+++ master/deploy/deploy.sh
@@ -1,1 +1,1 @@
-awk -F "," '{system("ssh root@"$1" \"echo "$2" > /etc/hostname;hostname;rm -rf *;apt-get install git;git clone https://github.com/php-afiliassur/phpmailer.git;cd phpmailer/slave/setup;chmod +x setup.sh;./setup.sh  \"")}' hosts
+awk -F "," '{system("ssh root@"$1" \"echo "$2" > /etc/hostname;hostname;rm -rf *;apt-get install git;git clone https://github.com/php-afiliassur/125243.git;cd 125243/slave/setup;chmod +x setup.sh;./setup.sh  \"")}' hosts
